cf7c1dfea20daab7603eb32b487fde3a2c415bee
AlejandroRDev <iron-man@avengers.com>
2021-12-12 14:35:19 +0100

    #Iteration-2*sin-acabar*

---
+++ iteracion2/iteracion2.js
@@ -0,0 +1,26 @@
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+
+
+
+function alumnoAproved() {
+  let alumno = [];
+  for (let i = 0; i < alumns.length; i++) {
+      alumno.push(alumns[i]);
+    for (let j = 0; j < alumno.length; j++) {
+       
+        
+    }
+  console.log(alumno);
+}
+
+}
+
+alumnoAproved();
+
+//** revisar no acabado. */
